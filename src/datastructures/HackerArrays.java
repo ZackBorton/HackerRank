b020481544c3a92b51a22bdd2cec37d7f52f1073
Zack Borton <zaborton@gmail.com>
2018-01-15 12:45:36 -0600

    basic 2d array additions, will add tests as well

--- src/datastructures/HackerArrays.java
+++ src/datastructures/HackerArrays.java
@@ -4,27 +4,6 @@ import java.util.Scanner;
 
 
 public class HackerArrays {
-
-    //TODO : Make this look pretty
-    private int[][] testCaseOne = {
-            {1, 1, 1, 0, 0, 0},
-            {0, 1, 0, 0, 0, 0},
-            {1, 1, 1, 0, 0, 0},
-            {0, 0, 2, 4, 4, 0},
-            {0, 0, 0, 2, 0, 0},
-            {0, 0, 1, 2, 4, 0}
-    };
-
-    private int[][] testCaseTwo = {
-            {-1, -1, 0, -9, -2, -2},
-            {-2, -1, -6, -8, -2, -5},
-            {-1, -1, -1, -2, -3, -4},
-            {-1, -9, -2, -4, -4, -5},
-            {-7, -3, -3, -2, -9, -9},
-            {-1, -3, -1, -2, -4, -5}
-    };
-
-
     /**
      * So basic its almost visual basic
      * <p>
@@ -89,6 +68,12 @@ public class HackerArrays {
         }
         System.out.println(maxsum);
     }
+
+    public void dynamicArray()
+    {
+        
+    }
+
 
     //TODO: Reprompt on incorrect input
     /**
--- tests/HackerArraysTest.java
+++ tests/HackerArraysTest.java
@@ -5,9 +5,34 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HackerArraysTest {
+
+    //TODO : Test cases for 2d array
+    private int[][] testCaseOne = {
+            {1, 1, 1, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0},
+            {1, 1, 1, 0, 0, 0},
+            {0, 0, 2, 4, 4, 0},
+            {0, 0, 0, 2, 0, 0},
+            {0, 0, 1, 2, 4, 0}
+    };
+
+    private int[][] testCaseTwo = {
+            {-1, -1, 0, -9, -2, -2},
+            {-2, -1, -6, -8, -2, -5},
+            {-1, -1, -1, -2, -3, -4},
+            {-1, -9, -2, -4, -4, -5},
+            {-7, -3, -3, -2, -9, -9},
+            {-1, -3, -1, -2, -4, -5}
+    };
+
+
     @Test
-    void twoDArray() {
+    void twoDArrayNegatives() {
 
     }
 
+    @Test
+    void twoDArrayPositives() {
+
+    }
 }
